62e25396735338181fe3cd80f145da9ce72281b8
Kaan <70149418+kaanwk@users.noreply.github.com>
2023-12-30 06:47:37 +0000

    new resume

--- app/resume/page.js
+++ app/resume/page.js
@@ -1,24 +1,28 @@
 import Image from 'next/image'
 import React from 'react';
-import styles from "../components/resumecomponent.css";
+import "../components/resumecomponent.css";
 
 
 
 export default function Projects() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div className={styles.mainContainer}>
-                <div className="mainContent">
-                    <center>
+
+        <div className="resume-div">
+        {/* <main className="flex min-h-screen flex-col items-center justify-between p-24 border"> */}
+            {/* <div className={styles.mainContainer}> */}
+                {/* <div className="mainContent">
+                    <center> */}
                         <object data=
                             "https://drive.google.com/file/d/1UbVDKOBjTqBmZrwOOA12HflfWaB6WEbA/preview"
                             width={500} height={620}>
                         </object>
-                    </center>
-                </div>
-            </div>
+                    {/* </center>
+                </div> */}
+            {/* </div> */}
 
+{/* 
+        </main> */}
 
-        </main>
+        </div>
     );
 }
