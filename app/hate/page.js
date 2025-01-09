186bef96b256c0cf8877ead9e85aeab528be96b5
Kaan <alpikaan.pamukcu@gmail.com>
2025-01-09 19:59:23 +0000

    hate post section lol

--- app/hate/page.js
+++ app/hate/page.js
@@ -6,32 +6,23 @@ import "../components/hatepostcomponent.css";
 
 export default function Hate() {
     return (
-
         <div className="hate-div">
-        {/* <main className="flex min-h-screen flex-col items-center justify-between p-24 border"> */}
-            {/* <div className={styles.mainContainer}> */}
-                {/* <div className="mainContent">
-                    <center> */}
-                    <p className="hiImKaan">&nbsp;</p>
-                    <div>list of my hate posts</div>
-                    <br></br>
-                    <div> disclaimer: </div>
-                    <div> i just love to hate, these are all my personal opinions</div>
-                    <div> please dont come for me </div>
-                    <br></br>
-                    <div className="hiImKaan" >•<a className="hiImKaan">Why "AI Companion" Companies Suck</a></div>
-                    <br></br>
-                        <object data=
-                            "https://drive.google.com/file/d/1iOe4jyBk4iGjBQ9Xj06BBl868kku71R-/preview"
-                            width={500} height={620}>
-                        </object>
-                    {/* </center>
-                </div> */}
-            {/* </div> */}
-
-{/* 
-        </main> */}
-
+            <p className="hiImKaan">&nbsp;</p>
+            <div>list of my hate posts</div>
+            <br />
+            <div> disclaimer: </div>
+            <div> i just love to hate, these are all my personal opinions</div>
+            <div> please don&apos;t come for me </div>
+            <br />
+            <div className="hiImKaan">
+                •<a className="hiImKaan">Why &quot;AI Companion&quot; Companies Suck</a> 
+            </div>
+            <br />
+            <object
+                data="https://drive.google.com/file/d/1iOe4jyBk4iGjBQ9Xj06BBl868kku71R-/preview"
+                width={500}
+                height={620}
+            />
         </div>
     );
 }
